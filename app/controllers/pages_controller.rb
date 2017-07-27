18614bc42f4c25e72e3fc52c751592cdfcf6a7cb
khendrikse <30577427+khendrikse@users.noreply.github.com>
2017-07-27 11:50:44 +0200

    Controller now creates the type of pdf I want.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,23 @@ class PagesController < ApplicationController
       format.pdf do
         render :pdf => "file_name",
         :template => 'pages/home.html.erb',
-        layout: 'application.html.erb',
-        disposition: 'attachment'
+        :layout => 'application.html.erb',
+        :disposition => 'attachment',
+        :orientation => 'Portrait',
+        :page_size => 'a3',
+        #:page_height => '5cm',
+        #:page_width => '5cm',
+        :disable_javascript => false,
+        :encoding => 'utf8',
+        :background => true,
+        :enable_plugins => true,
+        :disable_smart_shrinking => true,
+        :show_as_html => params.key?('debug'),
+        margin: { top: '0mm',
+                  bottom: '0mm',
+                  left: '0mm',
+                  right: '0mm'
+                }
       end
     end
   end
