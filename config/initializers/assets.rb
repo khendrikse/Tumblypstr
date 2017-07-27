57ecfdcae9fa4e6062235f9a914cb58ad54acf4c
khendrikse <30577427+khendrikse@users.noreply.github.com>
2017-07-27 14:30:20 +0200

    Cleaned up css and javascript

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,10 +2,11 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( style1.css )
+Rails.application.config.assets.precompile += %w( slideshow.css )
 Rails.application.config.assets.precompile += %w( normalize.css )
 Rails.application.config.assets.precompile += %w( skeleton.css )
-Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( overall_style.css )
+Rails.application.config.assets.precompile += %w( text_animation.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
